ab8be5c5a4a85f2505e11754e21aea74de98a6c6
Addis-Mendaye <amendaye@yahoo.com>
2021-03-09 18:08:14 -0600

    changes navbar hover color

--- assets/js/holsana.js
+++ assets/js/holsana.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
     $('.ex1').textyle();
-    $('.desc').textyle();
+    //$('.ex1').textyle();
     //you can select options or add callback
-    $('.ex2').textyle({
-        duration : 400,
-        delay : 100,
-        easing : 'swing',
-        callback : function(){
-            $(this).css({
-                color : 'coral',
-                transition : '1s',
-            });
-            $('.desc').css('opacity',1);
-        }
-    });
-        
+//    $('.ex1').textyle({
+//        duration : 400,
+//        delay : 100,
+//        easing : 'swing',
+//        callback : function(){
+//            $(this).css({
+//                color : 'coral',
+//                transition : '1s',
+//            });
+//            $('.desc').css('opacity',1);
+//        }
+//    });
+//        
     $('#idSubmit').on('click', function () {// Signin - existing user
         logOnConsole('click error : idSubmit');
         var _isEmailMobileFormValidated = $("#contactFormId").valid();
